b1ec165261baaae7fa53375d03490145b5bcd2d7
KRis20F <krigutierrez009@gmail.com>
2025-05-06 16:35:37 +0200

    fix: other more 3

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -12,13 +12,16 @@ export const metadata: Metadata = {
   title: 'Invoices',
 };
 
-type Props = {
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
+interface PageProps {
+  searchParams: Promise<{
+    [key: string]: string | string[] | undefined;
+  }>;
+}
 
-export default async function Page({ searchParams = {} }: Props) {
-  const query = typeof searchParams.query === 'string' ? searchParams.query : '';
-  const currentPage = typeof searchParams.page === 'string' ? Number(searchParams.page) : 1;
+export default async function Page({ searchParams }: PageProps) {
+  const params = await searchParams;
+  const query = typeof params.query === 'string' ? params.query : '';
+  const currentPage = typeof params.page === 'string' ? Number(params.page) : 1;
   const totalPages = await fetchInvoicesPages(query);
 
   return (
